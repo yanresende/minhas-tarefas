989c6f7b52506d6d54952cc38cba1876113b29af
Yan Araujo Resende <saresende555@gmail.com>
2024-03-14 17:29:51 -0300

    Commit inicial

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import tarefasReducer from './reducers/tarefas'
+
+import filtroReducers from './reducers/filtro'
+
+const store = configureStore({
+  reducer: {
+    tarefas: tarefasReducer,
+    filtro: filtroReducers
+  }
+})
+
+export type RootReducer = ReturnType<typeof store.getState>
+
+export default store
